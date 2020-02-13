30ae87bbc31f61548160f108408e907625e6493b
Wayne <enyaw0215@gmail.com>
2020-02-13 03:19:07 +0000

    jquery test

--- public/Inclass/js/index.js
+++ public/Inclass/js/index.js
@@ -3,5 +3,3 @@ $('div').css("backgroud-color", "purple");
 $('.highlight').css("width", "200px");
 $('#third').css("border-width","2px" );
 $('#third').css("border-color", "orange");
-
-console.log("test");
