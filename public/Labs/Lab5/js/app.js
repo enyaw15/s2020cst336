19e24cdd8b1d7b1c2492f7e1c1c3ef01957dcf0d
Wayne <enyaw0215@gmail.com>
2020-02-26 18:35:50 +0000

    added ajax request for images and display the images

--- public/Labs/Lab5/js/app.js
+++ public/Labs/Lab5/js/app.js
@@ -1,5 +1,27 @@
-console.log("connected");
+
 $("#search").on("click",function()
 {
-   console.log("click"); 
+    let keyword = $("#keyword").val();
+    keyword.replace(" ", "+");
+    let orientation = $("#orientation").val()==="h"?"horizontal":"vertical";
+    let imageDisplay = $("#image-display");
+    imageDisplay.html("");
+    $.ajax({
+        method: "GET",
+        url: "https://pixabay.com/api/",
+        dataType: "json",
+        data: { "key" :"15401070-3f0e62fc6ef63c9390f6fb50d" , "q": keyword, "orientation" : orientation},
+        success: function(result,status)
+        {
+            let data = result.hits;
+            data.forEach(function(img){
+                imageDisplay.append(`<div class = "col-sm"><img src = '${img.previewURL}' width = '${img.previewWidth}' height = '${img.previewHeight}'/></div>`);
+            });
+        },
+        error(err)
+        {
+            console.log(err);
+        }
+        
+    })
 });
